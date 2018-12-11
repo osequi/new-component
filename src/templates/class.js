b51fc13227780c77e5138d7cd72ab6a4bafa97c4
metamn <bartus.csongor@gmail.com>
2018-12-11 16:17:17 +0200

    Class template updated

--- src/templates/class.js
+++ src/templates/class.js
@@ -1,10 +1,61 @@
-import React, { Component } from 'react';
+/**
+ * Component description
+ *
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, {css} from 'styled-components';
 
 
-class COMPONENT_NAME extends Component {
-  render() {
-    return <div />;
-  }
+/**
+ * The loading container
+ */
+const Loading = styled.div``;
+
+
+/**
+ * The main container
+ */
+const Container = styled.div``;
+
+
+/**
+ * The main class
+ */
+class COMPONENT_NAME extends React.Component {
+	render() {
+		const {loading, className} = this.props;
+
+		if (loading) {
+			return (
+				<Loading className={className}>Loading ...</Loading>
+			)
+		}
+
+		return (
+			<Container className={className} />
+		)
+	}
 }
+
+
+/**
+ * The prop types
+ */
+COMPONENT_NAME.propTypes = {
+	/**
+	 * Component is loading?
+	 */
+	loading: PropTypes.bool,
+}
+
+
+/**
+ * Default props
+ */
+COMPONENT_NAME.defaultProps = {
+	loading: false,
+};
+
 
 export default COMPONENT_NAME;
